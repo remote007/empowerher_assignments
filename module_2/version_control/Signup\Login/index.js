f12cb939d16d4e91670596d910b1444402ae1f75
remote007 <pmnrai2020@gmail.com>
2024-12-08 21:15:34 +0530

    Q12 part1

--- module_2/version_control/Signup\Login/index.js
+++ module_2/version_control/Signup\Login/index.js
@@ -1,3 +1,12 @@
+//  Q12 part1
+// Create a repo named Signup/Login clone this repo with your local system,
+//  then create a file called index.js, then write function called signupthat 
+// takes a string as paramter called userName, declare an array in the function with 
+// some usernames already in that array, this function should return User Already Regsistered,
+//  Please Login if the userName already present in the array or push the userName in to the 
+// array and return Signup Sucessfull, Please Login,
+
+Once it is done, then add, commit and push into the repo.
 function signup(userName){
     usernames = ['abc','nitu','shaam','gulaab'];
     for(let i = 0 ; i < userName.length ; i++)
