cd19707026ef90ef761beb7720e5d27ae1655c50
remote007 <pmnrai2020@gmail.com>
2025-01-02 12:05:45 +0530

    Update main.js

--- module_2/Casserole Co./scripts/main.js
+++ module_2/Casserole Co./scripts/main.js
@@ -1,6 +1,6 @@
-import { loadNavbar } from 'navbar.js';
-import { loadMenu } from 'menu.js';
-import { loadFooter } from 'footer.js';
+import { loadNavbar } from '/navbar.js';
+import { loadMenu } from '/menu.js';
+import { loadFooter } from '/footer.js';
 
 window.onload = () => {
     loadNavbar();    // Dynamically load the navbar
