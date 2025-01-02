df8d84f18286eaa8ebb412fc35c54d01effe93f5
remote007 <pmnrai2020@gmail.com>
2025-01-02 12:26:49 +0530

    Delete module_2/Casserole Co. directory

--- module_2/Casserole Co./scripts/main.js
+++
@@ -1,9 +0,0 @@
-import { loadNavbar } from './navbar.js';
-import { loadMenu } from './menu.js';
-import { loadFooter } from './footer.js';
-
-window.onload = () => {
-    loadNavbar();    // Dynamically load the navbar
-    loadMenu();     // Fetch and load menu
-    loadFooter();    // Dynamically load the footer
-};
